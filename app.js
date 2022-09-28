2cf286405b1962e0b8fa03101f6ce9ac046a9d64
Valeria Rico <valeroxanarico@gmail.com>
2022-09-28 09:38:32 -0300

    fixing

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ server.listen(port);
 
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static('client/build'));
   // Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
-	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+	  res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
 	});
   }
 module.exports = app;
